95f20f1e89f39d019c59eedcb9ce3d87f3e42d28
mehranGulzar <m.gulzar@web.de>
2022-11-14 13:31:22 +0100

    Security Login (Deploy)

---
+++ backend/src/main/java/com/example/backend/Security.java
@@ -0,0 +1,19 @@
+package com.example.backend;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class Security extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("api/article").authenticated();
+
+    }
+}
